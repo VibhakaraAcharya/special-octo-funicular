06187aff0ac87d20b608a60623bcac383c4213e6
Vibhakara Acharya <vibhakara.acharya@gmail.com>
2018-10-11 17:20:27 +0530

    Push all files

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+print("Hello Again!!!!!!!!")
+print("NBO")
+# Start!
+
+
---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Lana'
+my_age = 31  # truth
+my_height = 64  # inches
+my_weight = 65  # Kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print("Let's talk about %s." % my_name)
+print("She's %d inches tall." % my_height)
+print("She's %d Kgs heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("She's got %s eyes and %s hair." % (my_eyes, my_hair))
+print("Her teeth are usually %s depending on chocolates." % my_teeth)
+
+#  this line is tricky, try to get it exactly right
+
+print("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
